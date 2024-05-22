d20edc55024f4031f7a0eff6e881449abb5c73f3
typedecker <ketchum080306@gmail.com>
2024-05-23 05:07:07 +0530

    test

--- bot.py
+++ bot.py
@@ -22,6 +22,8 @@ import ffmpeg
 
 datetime_date_format = '%a %d %b %Y, %I:%M:%S %p UTC time'
 CONFESSION_CHANNEL_ID = 1239309061585899572
+BOT_OWNER_ID = 568446269610000385
+PET_OWNER_GUILD = 1230967641200394302
 
 # -------------------------------
 app = Flask('')
@@ -71,12 +73,45 @@ bot = discord.Client(intents = intents)
 
 voice_client = None
 
+async def review_ping_check(members) :
+    REVIEW_PING_ROLE_ID = 1242796399754743808
+    REVIEW_PING_CHANNEL_ID = 1242796180359086130
+    
+    review_ping_channel = bot.fetch_channel(REVIEW_PING_CHANNEL_ID)
+    for member in members :
+        found_role_list = [role for role in member.roles if role.id == REVIEW_PING_ROLE_ID]
+        if any(found_role_list) :
+            await review_ping_channel.send(f'Hey {member.mention}! It would be great if you could post a review of our server on disboard :D! It helps us grow and bring new friends to the server faster ✨!!!\n https://disboard.org/review/create/1230967641200394302')
+            await member.remove_roles(found_role_list)
+    return
+
+@tasks.loop(hours = 12)
+async def bot_updatation() :
+    # Called every 12 hours and does all the timed updatation that the bot needs.
+    
+    guild = await bot.fetch_guild(PET_OWNER_GUILD)
+    members = guild.fetch_members()
+    
+    # Assign levels based on people's messages.
+    
+    # Checks for the review ping roles
+    review_ping_check(members)
+    return
+
 @bot.event
 async def on_ready() :
     bot_activity = discord.Game(name = 'Getting Slapped by the slappers!', start = bot.user.created_at)
     await bot.change_presence(activity = bot_activity)
     
     print('[LOG] Beam me up scotty ;)')
+    
+    try :
+        bot_owner = bot.get_user(BOT_OWNER_ID)
+        if bot_owner.dm_channel == None :
+            await bot_owner.create_dm()
+        await bot_owner.dm_channel.send('Gargling goblins bot has been deployed at {utc_time}'.format(utc_time = datetime.utcnow().strftime(datetime_date_format)))
+    except :
+        print('[on_ready func]: Ready action notif couldn\'t be sent to goblin sensei\'s owner.')
     return
 
 @bot.event
@@ -101,7 +136,7 @@ async def on_message(message) :
         return
     if message.content.startswith('$$confess ') :
         confession = message.content[len('$$confess') : ]
-        embed = discord.Embed(color = discord.Colour.dark_green(), title = 'Anonymous Confession', description = confession)
+        embed = discord.Embed(color = discord.Colour.from_str('#F05E22'), title = 'Anonymous Confession', description = confession)
         confession_channel = await bot.fetch_channel(CONFESSION_CHANNEL_ID)
         confession_msg = await confession_channel.send(embed = embed)
         if not isinstance(message.channel, discord.DMChannel) :
